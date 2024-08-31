37b9c42e2c82786613c69416691252436931f407
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-31 16:45:48 -0300

    add logging in customer resource

--- customer-api/src/main/java/com/gabrielluciano/customerapi/resources/CustomerResource.java
+++ customer-api/src/main/java/com/gabrielluciano/customerapi/resources/CustomerResource.java
@@ -4,6 +4,7 @@ import com.gabrielluciano.customerapi.domain.Customer;
 import com.gabrielluciano.customerapi.services.CustomerService;
 import com.gabrielluciano.customerapi.services.dto.CreateCustomerDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,18 +14,21 @@ import java.net.URI;
 @RestController
 @RequestMapping("customers")
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerResource {
 
     private final CustomerService service;
 
     @GetMapping
     public String status() {
+        log.info("Customer service status requested");
         return "ok";
     }
 
     @PostMapping
     public ResponseEntity<Void> save(@RequestBody CreateCustomerDTO createCustomerDTO) {
         Customer customer = service.save(createCustomerDTO);
+        log.info("Created customer with id '{}'", customer.getId());
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .query("cpf={cpf}")
                 .buildAndExpand(customer.getCpf())
